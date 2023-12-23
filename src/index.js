e2fa99c8c8874baa1281eda97377cd52edeb6d2d
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-12-23 18:57:17 +0300

    generated @syncfusion licence key and settings button

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 // index.js
 import React from "react";
 import { createRoot } from 'react-dom/client';
+import { registerLicense } from '@syncfusion/ej2-base';
 import "./index.css";
 import App from './App';
+registerLicense('ORg4AjUWIQA/Gnt2UVhhQlVFfV5dX2FWfFN0QXNYf1R0fV9GZEwgOX1dQl9gSHxRdURjXXZacHNWQGI=');
 
 const root = createRoot(document.getElementById('root'));
 const rootElement = <App />;
